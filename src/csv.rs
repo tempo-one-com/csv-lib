ea644a251e0cc2f3c331622733b267d0734d9204
Olivier Poppon <olivier.poppon@tempo-one.com>
2024-11-08 10:32:44 +0100

    ajout mode quote et option

--- src/csv.rs
+++ src/csv.rs
@@ -2,7 +2,7 @@ use chrono::NaiveDate;
 
 use crate::formater::{FormatType, Formater};
 
-type CellFormater<S, T> = Box<dyn Fn(&S) -> T>;
+//type PropGetter<S, T> = Fn(&S) -> T;
 
 pub struct Csv<F>
 where
@@ -16,11 +16,15 @@ impl<F> Csv<F>
 where
     F: Formater + Sized,
 {
-    pub fn serialize<S, T>(&self, values: &[S], columns: Vec<Cell<S, CellType>>) -> String {
+    pub fn serialize<S, T>(&self, values: &[S]) -> String
+    where
+        S: CsvStruct,
+    {
         let mut data = String::new();
         let mut header = vec![];
         let mut rows = vec![];
 
+        let mode = self.config.mode.clone();
         let field_separator = match self.config.separator {
             FieldSeparator::Comma => ",",
             FieldSeparator::SemiColumn => ";",
@@ -31,29 +35,63 @@ where
             Eol::Windows => "\r\n",
         };
 
-        for (i, item) in values.into_iter().enumerate() {
+        for (i, item) in values.iter().enumerate() {
             let mut row = vec![];
 
-            for col in &columns {
+            for cell in item.get_cells() {
                 if i == 0 {
-                    if let Some(title) = col.title.clone() {
-                        header.push(title);
+                    if let Some(title) = &cell.title {
+                        let value = match mode {
+                            QuoteMode::None => title.to_string(),
+                            _ => format!(r#""{title}""#),
+                        };
+
+                        header.push(value);
                     }
                 }
 
-                let value = match (col.getter)(item) {
-                    CellType::Date(v) => match self.formater.date() {
-                        FormatType::Date(f) => v.format(&f).to_string(),
-                        _ => unreachable!(),
-                    },
-                    CellType::Float(v) => match self.formater.float() {
-                        FormatType::Float(f) => {
-                            let float_str = format!("{:.3}", v);
-                            float_str.replace(".", &f)
+                let value = match &cell.value {
+                    CellType::Date(v) | CellType::DateOpt(Some(v)) => match self.formater.date() {
+                        FormatType::Date(f) => {
+                            let value = v.format(&f).to_string();
+
+                            match mode {
+                                QuoteMode::All => format!(r#""{value}""#),
+                                _ => value,
+                            }
                         }
                         _ => unreachable!(),
                     },
-                    CellType::String(v) => v,
+                    CellType::Float(v) | CellType::FloatOpt(Some(v)) => match self.formater.float()
+                    {
+                        FormatType::Float(f) => {
+                            let value = format!("{:.3}", v).replace(".", &f);
+
+                            match mode {
+                                QuoteMode::All => format!(r#""{value}""#),
+                                _ => value,
+                            }
+                        }
+                        _ => unreachable!(),
+                    },
+                    CellType::Float64(v) | CellType::FloatOpt64(Some(v)) => {
+                        match self.formater.float() {
+                            FormatType::Float(f) => {
+                                let value = format!("{:.3}", v).replace(".", &f);
+                                
+                                match mode {
+                                    QuoteMode::All => format!(r#""{value}""#),
+                                    _ => value,
+                                }                                    
+                            }
+                            _ => unreachable!(),
+                        }
+                    }
+                    CellType::String(v) | CellType::StringOpt(Some(v)) => match mode {
+                        QuoteMode::None => v.to_string(),
+                        _ => format!(r#""{v}""#),
+                    },
+                    _ => String::new(),
                 };
 
                 row.push(value);
@@ -66,14 +104,15 @@ where
         let header = header.join(field_separator);
 
         data.push_str(&header);
-        data.push_str(&eol);
-        let rows = rows.join(&eol);
+        data.push_str(eol);
+        let rows = rows.join(eol);
         data.push_str(&rows);
 
         data
     }
 }
 
+#[derive(Debug, Clone)]
 pub enum QuoteMode {
     None,
     Mixed,
@@ -92,17 +131,58 @@ pub enum FieldSeparator {
 
 pub enum CellType {
     String(String),
+    StringOpt(Option<String>),
+
     Date(NaiveDate),
+    DateOpt(Option<NaiveDate>),
+
     Float(f32),
+    FloatOpt(Option<f32>),
+    Float64(f64),
+    FloatOpt64(Option<f64>),
+
+    Int(i32),
+    IntOpt(Option<i32>),
 }
 
-pub struct Cell<S, T> {
+pub trait CsvStruct {
+    fn get_cells(&self) -> Vec<Cell>;
+}
+
+pub struct Cell {
     pub title: Option<String>,
-    pub getter: Box<dyn Fn(&S) -> T>,
+    pub value: CellType,
+}
+
+impl Cell {
+    pub fn new_title(title: &str, value: CellType) -> Self {
+        Self {
+            title: Some(title.to_string()),
+            value,
+        }
+    }
 }
 
 pub struct Config {
     pub eol: Eol,
     pub separator: FieldSeparator,
     pub mode: QuoteMode,
+}
+
+impl Config {
+    pub fn new_unix_fr() -> Self {
+        Self {
+            eol: Eol::Unix,
+            separator: FieldSeparator::SemiColumn,
+            mode: QuoteMode::Mixed,
+        }
+    }
+
+    pub fn new_unix_en() -> Self {
+        Self {
+            eol: Eol::Unix,
+            separator: FieldSeparator::Comma,
+            mode: QuoteMode::Mixed,
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,12 @@
 use chrono::NaiveDate;
+use csv::{Cell, CsvStruct};
 
 mod csv;
 mod formater;
 mod lang;
 
-#[derive(Debug, Default)]
-struct Test {
-    name: String,
-    date: NaiveDate,
-    removed_on: Option<NaiveDate>,
-    size: f32,
-}
+use csv::CellType;
+
 
 #[cfg(test)]
 mod tests {
@@ -20,13 +16,32 @@ mod tests {
 
     use super::*;
 
+    #[derive(Debug, Default)]
+    struct Test {
+        name: String,
+        date: NaiveDate,
+        removed_on: Option<NaiveDate>,
+        size: f32,
+    }
+    
+    impl CsvStruct for Test {
+        fn get_cells(&self) -> Vec<csv::Cell> {
+            vec![
+                Cell::new_title("Name", CellType::String(self.name.clone())),
+                Cell::new_title("Taille", CellType::Float(self.size)),
+                Cell::new_title("DOB", CellType::Date(self.date)),
+                Cell::new_title("DeletedOn", CellType::DateOpt(self.removed_on)),
+            ]
+        }
+    }
+    
     #[test]
     fn fr_linux() {
         let values = vec![
             Test {
                 name: "A".to_string(),
                 date: NaiveDate::from_ymd_opt(2020, 1, 28).unwrap(),
-                removed_on: Some(NaiveDate::from_ymd_opt(2024, 11, 07).unwrap()),
+                removed_on: Some(NaiveDate::from_ymd_opt(2024, 11, 7).unwrap()),
                 size: 1.79,
             },
             Test {
@@ -37,33 +52,14 @@ mod tests {
             },
         ];
 
-        let csv_cols = vec![
-            Cell {
-                title: Some("Name".to_string()),
-                getter: Box::new(|x: &Test| CellType::String(x.name.clone())),
-            },
-            Cell {
-                title: Some("Taille".to_string()),
-                getter: Box::new(|x: &Test| CellType::Float(x.size)),
-            },
-            Cell {
-                title: Some("DOB".to_string()),
-                getter: Box::new(|x: &Test| CellType::Date(x.date)),
-            },
-        ];
-
-        let config = Config {
-            eol: Eol::Unix,
-            separator: FieldSeparator::SemiColumn,
-            mode: QuoteMode::None,
-        };
+        let config = Config::new_unix_fr();
         let formater = FormatFr;
 
         let csv = Csv { config, formater };
-        let result = csv.serialize::<_, Test>(&values, csv_cols);
-        let expected = r#"Name;Taille;DOB
-A;1,790;28/01/2020
-B;1,750;11/10/1950"#;
+        let result = csv.serialize::<_, Test>(&values);
+        let expected = r#""Name";"Taille";"DOB";"DeletedOn"
+"A";1,790;28/01/2020;07/11/2024
+"B";1,750;11/10/1950;"#;
 
         assert_eq!(result, expected);
     }
@@ -74,7 +70,7 @@ B;1,750;11/10/1950"#;
             Test {
                 name: "A".to_string(),
                 date: NaiveDate::from_ymd_opt(2020, 1, 28).unwrap(),
-                removed_on: None,
+                removed_on: Some(NaiveDate::from_ymd_opt(2024, 11, 7).unwrap()),
                 size: 1.79,
             },
             Test {
@@ -85,34 +81,60 @@ B;1,750;11/10/1950"#;
             },
         ];
 
-        let csv_cols = vec![
-            Cell {
-                title: Some("Name".to_string()),
-                getter: Box::new(|x: &Test| CellType::String(x.name.clone())),
-            },
-            Cell {
-                title: Some("Taille".to_string()),
-                getter: Box::new(|x: &Test| CellType::Float(x.size)),
-            },
-            Cell {
-                title: Some("DOB".to_string()),
-                getter: Box::new(|x: &Test| CellType::Date(x.date)),
-            },
-        ];
-
         let config = Config {
             eol: Eol::Unix,
             separator: FieldSeparator::Comma,
-            mode: QuoteMode::None,
+            mode: QuoteMode::Mixed,
         };
         let formater = FormatEn;
 
         let csv = Csv { config, formater };
-        let result = csv.serialize::<_, Test>(&values, csv_cols);
-        let expected = r#"Name,Taille,DOB
-A,1.790,2020-01-28
-B,1.750,1950-10-11"#;
+        let result = csv.serialize::<_, Test>(&values);
+        let expected = r#""Name","Taille","DOB","DeletedOn"
+"A",1.790,2020-01-28,2024-11-07
+"B",1.750,1950-10-11,"#;
 
         assert_eq!(result, expected);
     }
+
+    #[test]
+    fn mode_none() {
+        let values = vec![
+            Test {
+                name: "A".to_string(),
+                ..Default::default()
+            },
+        ];
+
+        let config = Config::new_unix_fr();
+        let formater = FormatFr;
+
+        let csv = Csv { config, formater };
+        let result = csv.serialize::<_, Test>(&values);
+        let expected = r#""Name";"Taille";"DOB";"DeletedOn"
+"A";0,000;01/01/1970;"#;
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]    
+    fn mode_all() {
+        let values = vec![
+            Test {
+                name: "A".to_string(),
+                ..Default::default()
+            },
+        ];
+
+        let config = Config::new_unix_fr();
+        let formater = FormatFr;
+
+        let csv = Csv { config, formater };
+        let result = csv.serialize::<_, Test>(&values);
+        let expected = r#""Name";"Taille";"DOB";"DeletedOn"
+"A";0,000;01/01/1970;"#;
+
+        assert_eq!(result, expected);
+    }
+
 }
